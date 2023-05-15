ea350736346680089fc94bc5f876641b3d2760fe
Shan Akiraj <shan.akiraj@gmail.com>
2023-05-15 12:23:45 -0400

    project setup

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react"
+import Header from "./components/Header"
+import Meme from "./components/Meme"
 
 
 function App() {
