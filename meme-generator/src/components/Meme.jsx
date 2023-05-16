cd41b6d9ddf0e9a3777a7c43765b2e37012689c8
Shan Akiraj <shan.akiraj@gmail.com>
2023-05-16 12:35:48 -0400

    created state for meme

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -2,6 +2,12 @@ import React from "react"
 
 export default function Meme() {
 
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+
     return (
         <main>
             <div className="form">
@@ -25,6 +31,11 @@ export default function Meme() {
                 >
                     Get a new meme image 
                 </button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
         </main>
     )
