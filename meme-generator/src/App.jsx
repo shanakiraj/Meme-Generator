f972b750c7d642ceb23deb74465a5b9207fcaa5b
Shan Akiraj <shan.akiraj@gmail.com>
2023-05-16 12:25:25 -0400

    set up header and meme form

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -3,9 +3,17 @@ import Header from "./components/Header"
 import Meme from "./components/Meme"
 
 
-function App() {
+export default function App() {
+    return (
+        <div>
+             <Header />
+             <Meme />
+            
+        </div>
+       
+    )
 
 
 }
 
-export default App
+
---
+++ meme-generator/src/components/Meme.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+
+export default function Meme() {
+
+    return (
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                   
+                />
+                <button 
+                    className="form--button"
+                   
+                >
+                    Get a new meme image 
+                </button>
+            </div>
+        </main>
+    )
+}
