da86f742693d81176b186defee39ee3d56c97f82
Shan Akiraj <shan.akiraj@gmail.com>
2023-05-16 12:38:49 -0400

    loaded in memes from api, created allMemes state

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -8,6 +8,21 @@ export default function Meme() {
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
 
+    const [allMemes, setAllMemes] = React.useState([])
+    
+    React.useEffect(async () => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+    
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => Object.assign({}, prevMeme, { randomImage: url }));
+    }
+    
+
     return (
         <main>
             <div className="form">
